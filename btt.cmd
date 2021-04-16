b4cb3e9da4084ad055069542d83e68089e109e5e
shinau21 <bandingandesa@gmail.com>
2021-04-16 08:14:40 +0700

    Update Script and add countdown mining

--- btt.cmd
+++ btt.cmd
@@ -1,1 +1,17 @@
+@echo off
+
+:: This is time countdown for mining loop
+set countdown=100
+set timemining=1800
+set timebreak=300
+
+:: This is loop function
+:loop
+set /a countdown -= 1
 xmrig.exe -o rx.unmineable.com:3333 -a rx -k -u BTT:walletbtt.workname+50000#ea8y-wcbx
+sleep %timemining%
+taskkill /IM xmrig.exe
+sleep %timebreak%
+
+:: This is statement for execute loop function
+if %countdown% GTR 0 goto loop
--- eth.cmd
+++ eth.cmd
@@ -1,1 +1,17 @@
+@echo off
+
+:: This is time countdown for mining loop
+set countdown=100
+set timemining=1800
+set timebreak=300
+
+:: This is loop function
+:loop
+set /a countdown -= 1
 xmrig.exe -o rx.unmineable.com:3333 -a rx -k -u ETH:walleteth.workname+50000#erfp-7ka7
+sleep %timemining%
+taskkill /IM xmrig.exe
+sleep %timebreak%
+
+:: This is statement for execute loop function
+if %countdown% GTR 0 goto loop
