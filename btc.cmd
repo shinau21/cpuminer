a93dec7524769d6dd06d692e48cf71aa8b5efdf4
shinau21 <bandingandesa@gmail.com>
2021-04-16 17:25:39 +0700

    Fixing

--- btc.cmd
+++ btc.cmd
@@ -11,8 +11,11 @@ set timebreak=300
 :: This is loop function
 :loop
 set /a countdown -= 1
-xmrig.exe -o rx.unmineable.com:3333 -a rx -k -u BTC:%btcwallet%.%worker%+50000#esqb-onxi && timeout /t %timemining%
-taskkill /IM xmrig.exe && timeout /t %timebreak%
+xmrig.exe -o rx.unmineable.com:3333 -a rx -k -u BTC:%btcwallet%.%worker%+50000#esqb-onxi
+timeout /t %timemining%
+taskkill /IM xmrig.exe
+timeout /t %timebreak%
+
 
 :: This is statement for execute loop function
 if %countdown% GTR 0 goto loop
--- btt.cmd
+++ btt.cmd
@@ -11,8 +11,10 @@ set timebreak=300
 :: This is loop function
 :loop
 set /a countdown -= 1
-xmrig.exe -o rx.unmineable.com:3333 -a rx -k -u BTT:%bttwallet%.%worker%+50000#ea8y-wcbx && timeout /t %timemining%
-taskkill /IM xmrig.exe && timeout /t %timebreak%
+xmrig.exe -o rx.unmineable.com:3333 -a rx -k -u BTT:%bttwallet%.%worker%+50000#ea8y-wcbx
+timeout /t %timemining%
+taskkill /IM xmrig.exe
+timeout /t %timebreak%
 
 :: This is statement for execute loop function
 if %countdown% GTR 0 goto loop
