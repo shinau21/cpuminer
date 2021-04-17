6b871ab1d1451f70c0c50a41560f69b86f8ff126
shinau21 <bandingandesa@gmail.com>
2021-04-17 10:31:06 +0700

    Downgrade to version 1.0 but problem on another function

--- bnb.cmd
+++ bnb.cmd
@@ -5,18 +5,5 @@
 :: Example walletbnb:memo
 set bnbwallet=""
 set worker=""
-:: This is time countdown for mining loop
-set countdown=100
-set timemining=1800
-set timebreak=300
 
-:: This is loop function
-:loop
-set /a countdown -= 1
 xmrig.exe -o rx.unmineable.com:3333 -a rx -k -u BNB:%bnbwallet%.%worker%+50000#7bzi-layq
-timeout /t %timemining%
-taskkill /IM xmrig.exe
-timeout /t %timebreak%
-
-:: This is statement for execute loop function
-if %countdown% GTR 0 goto loop
